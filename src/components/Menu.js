e821a0aba2e364a71cff2994e5e5e9889df1ffcc
Krzysztof Stankiewicz <krzystan@wp.pl>
2022-07-26 22:59:17 +0200

    feat: add navigation

---
+++ src/components/Menu.js
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import { forwardRef } from 'react';
+import { useRouter } from 'next/router';
+
+const NavLink = forwardRef(({href, onClick, children}, ref) => {
+  const router=useRouter();
+  console.log(router);
+  return (
+	  <a href={href} onClick={onClick} ref={ref} style={{color: router.pathname===href?'red': 'black'}}>{children}</a>
+  )
+});
+const Menu = () => {
+  return (
+	  <nav>
+		<Link href="/" passHref>
+		  <NavLink>Home</NavLink>
+		</Link>
+		<Link href="/about" passHref>
+		  <NavLink>About</NavLink>
+		</Link>
+		<Link href="/contact" passHref>
+		  <NavLink>Contact</NavLink>
+		</Link>
+	  </nav>
+  );
+}
+export default Menu;
