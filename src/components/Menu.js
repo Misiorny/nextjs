cc21155138bc65fdad3519ba229e2c3bb3365578
Krzysztof Stankiewicz <krzystan@wp.pl>
2022-07-27 11:51:28 +0200

    feat: add first layout, add fetch data, finish menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,10 +3,10 @@ import { forwardRef } from 'react';
 import { useRouter } from 'next/router';
 
 const NavLink = forwardRef(({href, onClick, children}, ref) => {
-  const router=useRouter();
+  const router = useRouter();
   console.log(router);
   return (
-	  <a href={href} onClick={onClick} ref={ref} style={{color: router.pathname===href?'red': 'black'}}>{children}</a>
+	  <a href={href} onClick={onClick} ref={ref} style={{padding: 4, color: router.pathname === href ? 'red' : 'black'}}>{children}</a>
   )
 });
 const Menu = () => {
@@ -17,6 +17,9 @@ const Menu = () => {
 		</Link>
 		<Link href="/about" passHref>
 		  <NavLink>About</NavLink>
+		</Link>
+		<Link href="/posts" passHref>
+		  <NavLink>Posts</NavLink>
 		</Link>
 		<Link href="/contact" passHref>
 		  <NavLink>Contact</NavLink>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import MainLayout from '../components/layouts/Main';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+  );
 }
 
 export default MyApp
